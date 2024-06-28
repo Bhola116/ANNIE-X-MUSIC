0a3a605291099826d17b7040227fd909f0d13d76
soham6540 <savitafarkade6540@gmail.com>
2024-06-28 07:31:55 +0530

    Update logo.py

--- ANNIEMUSIC/plugins/misc/logo.py
+++ ANNIEMUSIC/plugins/misc/logo.py
@@ -14,7 +14,7 @@ async def logo(app, msg: Message):
     if len(msg.command) == 1:
        return await msg.reply_text("Usage:\n\n /logo jarvis")
     logo_name = msg.text.split(" ", 1)[1]
-    API = f"https://i.waifu.pics/Tj6Wzwo.png?text={logo_name}"
+    API = f"https://api.waifu.pics/sfw/waifu?text={logo_name}"
     req = requests.get(API).url
     await msg.reply_photo(
         photo=f"{req}")
